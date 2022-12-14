c228cf0ee664a7ec8ad81b210c5c88106e00d7c1
hduperier <59696097+hduperier@users.noreply.github.com>
2022-12-14 13:47:42 -0500

    Add files via upload

---
+++ A4/A4vars.py
@@ -0,0 +1,12 @@
+N = 1048576
+P = 1024
+M = 2000
+
+Cdisk = 2 # TB
+Csurf = 128 # GB
+Csect = 1024 # B
+Cblock = 8192 # B
+St = 2**14 # sectors per track
+Tseek = 0.006
+Trotation = 0.010
+Nfile = 2000000
